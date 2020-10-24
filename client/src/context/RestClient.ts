d0607bf83b4a619f37e6cb2490c7a6bd36204576
cyberboy1551 <blacy1551@gmail.com>
2020-10-24 01:26:29 -0500

    Build 6

--- client/src/context/RestClient.ts
+++ client/src/context/RestClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { IUser } from "../constants";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "http://127.0.0.1:5000";
 
 export const Login = async (
   username: string,
